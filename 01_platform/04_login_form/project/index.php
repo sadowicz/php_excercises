e80678e846133c57f17558821130a11b992f4dbf
Filip Sądowicz <sadowicz31@gmail.com>
2020-10-14 09:50:51 +0200

    Adding 01.1 Pierwszy Skrypt Solution

--- 01_platform/04_login_form/project/index.php
+++ 01_platform/04_login_form/project/index.php
@@ -1,3 +1,36 @@
-<?php
+<!DOCTYPE HTML>
+<html>
+    <body>
+        <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST")
+        {
+            $user = $_POST["usr"];
+            $password = $_POST["passwd"];
 
-// TODO: Implement me!
+            if($user == "" || $password == "")
+            {
+                echo 'EMPTY';
+            }
+            elseif($user != "foo" || $password != "foo123")
+            {
+                echo 'ERROR';
+            }
+            else
+            {
+                echo 'OK';
+            }
+        }
+        else
+        {
+            echo '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'">
+            <label for="usr">User: </label>
+            <input type="text" id="usr" name="usr"><br>
+            <label for="passwd">Password: </label>
+            <input type="password" id="passwd" name="passwd"><br>
+            <input type="submit" value="Login">
+        </form>';
+        }
+        ?>
+    </body>
+</html>
+
