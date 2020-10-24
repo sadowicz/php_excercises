b9830cbc9727824b49855548457dc6572d1a95da
Filip Sądowicz <sadowicz31@gmail.com>
2020-10-24 19:04:46 +0200

    02_chessboard SizeCept & ColorsCept & PatternCept passed

--- 02_superglobals/02_chessboard/project/index.php
+++ 02_superglobals/02_chessboard/project/index.php
@@ -44,12 +44,32 @@ session_start();
 <body>
 <?php
 
+    function line($x, $z, $dx, $dz, $b){
+        return $dz * $x - $dx * $z + $b * $dx;
+    }
+
     if(!isset($_SESSION["sx"])){
         $_SESSION["sx"] = 10;
     }
 
     if(!isset($_SESSION["sz"])){
         $_SESSION["sz"] = 10;
+    }
+
+    if(!isset($_SESSION["startX"])) {
+        $_SESSION["startX"] = -1;
+    }
+
+    if(!isset($_SESSION["startZ"])) {
+        $_SESSION["startZ"] = -1;
+    }
+
+    if(!isset($_SESSION["endX"])) {
+        $_SESSION["endX"] = -1;
+    }
+
+    if(!isset($_SESSION["endZ"])) {
+        $_SESSION["endZ"] = -1;
     }
 
     $color = "gray";
@@ -71,10 +91,99 @@ session_start();
         $color = $_COOKIE["color"];
     }
 
+    if(!isset($_SESSION["patternArr"])){
+        $_SESSION["patternArr"] = array_fill(0, $_SESSION["sz"],
+            array_fill(0, $_SESSION["sx"], $color)); // 2D array with colors of each <a> block
+    }
+    else{
+        for($row = 0; $row < $_SESSION["sz"]; $row++){
+            for($col = 0; $col < $_SESSION["sx"]; $col++){
+                if($_SESSION["patternArr"][$row][$col] != "white"){
+                    $_SESSION["patternArr"][$row][$col] = $color;
+                }
+            }
+        }
+    }
+
+    if($_SERVER["REQUEST_METHOD"] == "GET"){
+        if(isset($_GET["x"]) && isset($_GET["z"])){
+            if ($_SESSION["startX"] == -1 && $_SESSION["startZ"] == -1){
+                $_SESSION["startX"] = $_GET["x"];
+                $_SESSION["startZ"] = $_GET["z"];
+            }
+            else{
+                $_SESSION["endX"] = $_GET["x"];
+                $_SESSION["endZ"] = $_GET["z"];
+            }
+        }
+    }
+
+
+
+    if($_SESSION["startX"] != -1 && $_SESSION["startZ"] != -1 && $_SESSION["endX"] != -1 && $_SESSION["endZ"] != -1){
+        // Draw line into patternArr:
+        $x1 = $_SESSION["startX"];
+        $z1 = $_SESSION["startZ"];
+
+        $x2 = $_SESSION["endX"];
+        $z2 = $_SESSION["endZ"];
+
+        if($x1 <= $x2){
+            $kx = 1;
+            $dx = $x2 - $x1;
+        }
+        else{
+            $kx = -1;
+            $dx = $x1 - $x2;
+        }
+
+        if($z1 <= $z2){
+            $kz = 1;
+            $dz = $z2 - $z1;
+        }
+        else{
+            $kz = -1;
+            $dz = $z1 - $z2;
+        }
+
+        $_SESSION["patternArr"][$z1][$x1] = "white";
+
+        if($dx >= $dz){
+            $e = $dx / 2;
+            for($i = 0; $i < $dx; $i++){
+                $x1 += $kx;
+                $e -= $dz;
+                if($e < 0){
+                    $z1 += $kz;
+                    $e += $dx;
+                }
+                $_SESSION["patternArr"][$z1][$x1] = "white";
+            }
+        }
+        else{
+            $e = $dz / 2;
+            for($i = 0; $i < $dz; $i++){
+                $z1 += $kz;
+                $e -= $dx;
+                if($e < 0){
+                    $x1 += $kx;
+                    $e += $dz;
+                }
+                $_SESSION["patternArr"][$z1][$x1] = "white";
+            }
+        }
+
+        //Reset line start and end coords to -1
+        $_SESSION["startX"] = -1;
+        $_SESSION["startZ"] = -1;
+        $_SESSION["endX"] = -1;
+        $_SESSION["endZ"] = -1;
+    }
+
     for($rows = 0; $rows < $_SESSION["sz"]; $rows++){
         echo "<div>";
         for($cols = 0; $cols < $_SESSION["sx"]; $cols++){
-            echo '<a class="block '.$color.'" href="?x='.$cols.'&z='.$rows.'"></a>';
+            echo '<a class="block '.$_SESSION["patternArr"][$rows][$cols].'" href="?x='.$cols.'&z='.$rows.'"></a>';
         }
         echo "</div>";
     }
