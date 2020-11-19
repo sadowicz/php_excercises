c46e03243369bbfce4dcc62fd3936e2b512e6117
Filip Sądowicz <sadowicz31@gmail.com>
2020-11-19 14:15:41 +0100

    SQLiteStorage working

--- 05_data_objects/03_exercise/project/src/Storage/MySQLStorage.php
+++ 05_data_objects/03_exercise/project/src/Storage/MySQLStorage.php
@@ -5,13 +5,12 @@ namespace Storage;
 use Concept\Distinguishable;
 use PDO;
 
-class MySQLStorage implements Storage
-{
+class MySQLStorage implements Storage {
+
     private PDO $pdo;
     private \PDOStatement $insert;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->pdo = new PDO("mysql:host=127.0.0.1;port=3306;dbname=test", "test", "test123");
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -21,16 +20,13 @@ class MySQLStorage implements Storage
         $this->insert = $this->pdo->prepare("INSERT INTO objects VALUES (:key, :data)");
     }
 
-    public function store(Distinguishable $distinguishable) : void
-    {
+    public function store(Distinguishable $distinguishable) : void {
         $this->insert->bindValue('key', $distinguishable->key());
         $this->insert->bindValue('data', serialize($distinguishable));
         $this->insert->execute();
-
     }
 
-    public function loadAll(): array
-    {
+    public function loadAll(): array {
         $query = $this->pdo->query("SELECT `data` FROM objects");
         return $query->fetchAll(PDO::FETCH_FUNC, "unserialize");
     }
