caa1fdcdb226be207eb52bb637347e89c4de28bf
Filip Sądowicz <sadowicz31@gmail.com>
2020-11-19 15:28:38 +0100

    SessionStorage working

--- 05_data_objects/03_exercise/project/src/Storage/SessionStorage.php
+++ 05_data_objects/03_exercise/project/src/Storage/SessionStorage.php
@@ -6,20 +6,22 @@ use Concept\Distinguishable;
 
 class SessionStorage implements Storage
 {
-    public function __construct()
-    {
-        // TODO: ...
+    public function __construct() {
+        session_start();
+        $_SESSION["storage"] = [];
     }
 
-    public function store(Distinguishable $distinguishable) : void
-    {
-        // TODO: ...
+    public function store(Distinguishable $distinguishable) : void {
+        $_SESSION["storage"][$distinguishable->key()] = serialize($distinguishable);
     }
 
-    public function loadAll(): array
-    {
-        // TODO: ...
-        return [];
+    public function loadAll(): array {
+        $result = [];
 
+        foreach ($_SESSION["storage"] as $stored) {
+            $result[] = unserialize($stored);
+        }
+
+        return $result;
     }
 }
