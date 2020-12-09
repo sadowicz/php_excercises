8f850be7b2321926f9d513d07f190ffc74f30b30
Filip Sądowicz <sadowicz31@gmail.com>
2020-12-09 14:48:03 +0100

    Editor tests passed.

--- 07_testing/02_exercise/project/src/String/Editor.php
+++ 07_testing/02_exercise/project/src/String/Editor.php
@@ -4,5 +4,54 @@ namespace String;
 
 class Editor
 {
-    // TODO: ...
+    private string $text;
+
+    public function __construct(string $text) {
+
+        $this->text = $text;
+    }
+
+    public function get() : string {
+
+        return $this->text;
+    }
+
+    public function replace(string $search, string $replace) : object {
+
+        $this->text = str_replace($search, $replace, $this->text);
+        return $this;
+    }
+
+    public function lower() : object {
+
+        $this->text = strtolower($this->text);
+        return $this;
+    }
+
+    public function upper() : object {
+
+        $this->text = strtoupper($this->text);
+        return $this;
+    }
+
+    public function censor(string $censored) : object {
+
+        $replace = str_repeat('*', strlen($censored));
+        $this->text = str_replace($censored, $replace, $this->text);
+
+        return $this;
+    }
+
+    public function remove(string $removed) : object {
+
+        return $this->replace($removed, '');
+    }
+
+    public function repeat(string $repeated, int $repeats) : object {
+
+        $repeat = str_repeat($repeated, $repeats);
+        $this->text = str_replace($repeated, $repeat, $this->text);
+
+        return $this;
+    }
 }
