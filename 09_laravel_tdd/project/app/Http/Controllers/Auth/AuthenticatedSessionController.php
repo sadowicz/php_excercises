cbc255ec4dab1656d75de74aa546260c96837a15
Filip Sądowicz <sadowicz31@gmail.com>
2021-01-01 13:23:51 +0100

    Adding books creation (fixed redirections)

--- 09_laravel_tdd/project/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ 09_laravel_tdd/project/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -32,7 +32,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended("/books");
+        return redirect()->intended(RouteServiceProvider::HOME);
     }
 
     /**
