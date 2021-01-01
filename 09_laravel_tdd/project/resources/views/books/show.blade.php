14cb5a880b61498a32b1c1e289bc7e9f70875a71
Filip Sądowicz <sadowicz31@gmail.com>
2021-01-01 14:54:06 +0100

    Adding markdown interpretation (book show)

--- 09_laravel_tdd/project/resources/views/books/show.blade.php
+++ 09_laravel_tdd/project/resources/views/books/show.blade.php
@@ -1,4 +1,6 @@
+<?php //use GrahamCampbell\Markdown\Facades\Markdown; ?>
+
 <h2>Viewing a book</h2>
 <li>ISBN: {{$book->isbn}}</li>
 <li>Title: {{$book->title}}</li>
-<li>Description: {{$book->description}}</li>
+<li>Description: @markdown {{$book->description}} @endmarkdown</li>
